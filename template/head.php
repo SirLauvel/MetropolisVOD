77a6ee234f18e35645a40476b55e964560db445c
SirLauvel <84542996+SirLauvel@users.noreply.github.com>
2023-03-15 11:18:09 +0100

    Init_v2

---
+++ template/head.php
@@ -0,0 +1,20 @@
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <!-- jquuery -->
+  <script src="assets/js/jquery.min.js"></script>
+  <!-- Tailwind -->
+  <script src="https://cdn.tailwindcss.com"></script>
+  <script>
+    tailwind.config = {
+      theme: {
+        extend: {
+          colors: {
+            primary: '#FF6B00',
+          }
+        }
+      }
+    }
+  </script>
+  <title>MétropolisVOD</title>
+</head>
