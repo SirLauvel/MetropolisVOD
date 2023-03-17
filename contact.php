5fd6d2221fe01555e3613c659fc2aa09e44f471b
SirLauvel <84542996+SirLauvel@users.noreply.github.com>
2023-03-17 10:55:40 +0100

    Organisation_v0.1

---
+++ contact.php
@@ -0,0 +1,11 @@
+<!doctype html>
+<html>
+  <?php require('template/head.php');?>
+<body class="bg-primary">
+<?php require('template/navbar.php');?>
+
+
+
+<?php require('template/footer.php');?>
+</body>
+</html>
