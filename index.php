f3765e41cdf4c1d85370ac8df0fbd37b9891eeb8
SirLauvel <84542996+SirLauvel@users.noreply.github.com>
2023-03-19 14:13:53 +0100

    account_v1

--- index.php
+++ index.php
@@ -3,8 +3,10 @@
 <?php require('template/head.php');?>
 <body class="bg-primary">
 <?php require('template/navbar.php');?>
+
 <?php require('template/page/header.php');?>
 <?php require('template/page/mainHome.php');?>
+
 <?php require('template/footer.php');?>
 </body>
 </html>
