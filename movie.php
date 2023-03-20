c6f53c10c43dc61346063f83bfadb9e15f134ca4
SirLauvel <84542996+SirLauvel@users.noreply.github.com>
2023-03-20 14:19:28 +0100

    movie_v1

--- movie.php
+++ movie.php
@@ -4,38 +4,103 @@
 
 <body class="bg-primary">
     <?php require('template/navbar.php'); ?>
-    <nav class="bg-black px-10 py-2.5 w-full"> <!--  fixed top-0 left-0 z-10 -->
-        <div class="flex flex-wrap items-center justify-between">
+    <main class="min-h-[90vh] p-10">
+        <div class="pb-10">
+            <video class="w-full" autoplay muted controls>
+                <source src="assets/video/DemonSlayer_1.mp4" type="video/mp4">
+                Your browser does not support the video tag.
+            </video>
 
-            <div class="pb-5 flex flex-col gap-5 sm:flex-row sm:gap-10">
-                <h2 class="text-white text-3xl font-bold">Film</h2>
-                <?php require('assets/src/component/categoryMenu.php'); ?>
+        </div>
+
+        <div class="">
+            <p class="text-gray-400 text-md  "><a href="#">Animation</a>, <a href="#">Action</a>, <a
+                    href="#">Fantastique</a></p>
+            <div class="flex flex-col md:flex-row md:items-center md:justify-between">
+                <h1 class="text-white text-4xl pb-5">Demon Slayer: Kimetsu no Yaiba, le film : Le Train de l'Infini</h1>
+                <div class="flex items-center">
+                    <p class="ml-2 text-md font-bold text-white">4.95</p>
+                    <svg aria-hidden="true" class="w-10 h-10" fill="#FF6B00" viewBox="0 0 20 20"
+                        xmlns="http://www.w3.org/2000/svg">
+                        <title>Rating star</title>
+                        <path
+                            d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z">
+                        </path>
+                    </svg>
+                </div>
+            </div>
+
+
+            <p class="w-3/4 md:w-1/2 text-white text-md text-justify pb-20">
+                Après avoir terminé leur rééducation et entraînement au
+                domaine
+                des
+                papillons, Tanjirō, Nezuko, Zenitsu et Inosuke montent à bord du train de l'Infini afin de
+                rencontrer le
+                pilier de la Flamme, Kyōjurō Rengoku, et l'assister dans sa mission pour éliminer un démon ayant
+                fait
+                plus
+                de 40 victimes.</p>
+        </div>
+        <div class="w-full pb-10 flex flex-col items-center">
+            <div>
+                <div class="w-full text-xl font-medium text-center text-gray-500">
+                    <ul class="flex flex-wrap -mb-px" id="movieTab" data-tabs-toggle="#movieTabContent" role="tablist">
+                        <li class="mr-2" role="presentation">
+                            <button href="#" class="inline-block text-white p-4 rounded-t-lg border-b-2"
+                                id="associated-tab" data-tabs-target="#associated" type="button" role="tab"
+                                aria-controls="associated" aria-selected="false">Contenue
+                                Associé</button>
+                        </li>
+                        <li class="mr-2" role="presentation">
+                            <button href="#" class="inline-block text-white p-4 rounded-t-lg border-b-2" id="detail-tab"
+                                data-tabs-target="#detail" type="button" role="tab" aria-controls="detail"
+                                aria-selected="false" aria-current="page">Détails</button>
+                        </li>
+                        <li class="mr-2" role="presentation">
+                            <button href="#" class="inline-block text-white p-4 rounded-t-lg border-b-2" id="actor-tab"
+                                data-tabs-target="#actor" type="button" role="tab" aria-controls="actor"
+                                aria-selected="false">Acteur
+                            </button>
+                        </li>
+                        <li class="mr-2" role="presentation">
+                            <button href="#" class="inline-block text-white p-4 rounded-t-lg border-b-2 "
+                                id="comment-tab" data-tabs-target="#comment" type="button" role="tab"
+                                aria-controls="comment" aria-selected="false">Commentaire
+                            </button>
+                        </li>
+                    </ul>
+                </div>
             </div>
         </div>
-    </nav>
+        <div id="movieTabContent">
 
-    <main class="min-h-[90vh] p-10">
-
-        <div id="filmFlex" class="flex flex-row justify-center flex-wrap gap-3">
-            <a href="#"><img src="assets/img/film/Animation/one-piece-film-red.jpg" alt="affiche_OnePieceRed"
-                    width="150px" height="auto" /></a>
-            <a href="#"><img src="assets/img/film/Marvel/antman1.jfif" alt="" width="150px" height="auto" /></a>
-            <a href="#"><img src="assets/img/film/Marvel/antman2.jpg" alt="" width="150px" height="auto" /></a>
-            <a href="#"><img src="assets/img/film/Marvel/avengers1.jfif" alt="" width="150px" height="auto" /></a>
-            <a href="#"><img src="assets/img/film/Marvel/avengers2.jfif" alt="" width="150px" height="auto" /></a>
-            <a href="#"><img src="assets/img/film/Marvel/avengers3.jfif" alt="" width="150px" height="auto" /></a>
-            <a href="#"><img src="assets/img/film/Marvel/avengers4.jfif" alt="" width="150px" height="auto" /></a>
-        </div>
-        <div class="filmSlider">
-            <div class="pb-8">
-                <h2 class="pb-2 text-white text-2xl font-bold">Action</h2>
-                <?php require('assets/src/component/titleBar.php'); ?>
+            <div class="text-white" id="associated" role="tabpanel" aria-labelledby="associated-tab">
                 <?php require('assets/src/component/slider.php'); ?>
             </div>
+            <div class="text-white" id="detail" role="tabpanel" aria-labelledby="detail-tab">
+                <ul>
+                    <li class="pb-5 text-white font-bold text-2xl">Réalisateur</li>
+                    <li class="pb-5 text-white text-lg"><a href="https://fr.wikipedia.org/wiki/Haruo_Sotozaki"
+                            target="_blank">Haruo Sotozaki</a></li>
+
+                    <li class="pb-5 text-white font-bold text-2xl">Producteur</li>
+                    <li class="pb-5 text-white text-lg">Akifumi Fujio,Masanori Miyake, Yūma Takahashi </li>
+
+                    <li class="pb-5 text-white font-bold text-2xl">Avertissement relatif au contenu</li>
+                    <li class="pb-5 text-white text-lg">Des scènes, des propos ou des images peuvent heurter la
+                        sensibilité des spectateurs</li>
+
+                </ul>
+            </div>
+            <div class="text-white" id="actor" role="tabpanel" aria-labelledby="actor-tab">
+                <?php require('assets/src/component/actorSlider.php'); ?>
+            </div>
+            <div class="text-white" id="comment" role="tabpanel" aria-labelledby="comment-tab">
+                content #4
+            </div>
         </div>
-
     </main>
-
     <?php require('template/footer.php'); ?>
 </body>
 
--- template/footer.php
+++ template/footer.php
@@ -13,9 +13,11 @@
                 <h5 class="text-center text-white font-bold text-2xl pb-2 lg:text-xl">Cinéma</h5>
                 <ul class="lg:flex gap-3">
                     <li><a href="https://www.cinemet.fr/" class="underline" target="_blank">Métropolis Cinema</a></li>
-                    <li><a href="https://www.cinemet.fr/films-a-l-affiche/" class="underline" target="_blank">A l'affiche</a></li>
+                    <li><a href="https://www.cinemet.fr/films-a-l-affiche/" class="underline" target="_blank">A
+                            l'affiche</a></li>
                     <li><a href="https://www.cinemet.fr/horaires/" class="underline" target="_blank">Horraire</a></li>
             </div>
         </div>
         <p class="text-center text-white">©2023 MétropolisVOD. All Rights Reserved.</p>
 </footer>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/flowbite/1.6.4/flowbite.min.js"></script>
